4604b28fce523abf4c621e94a9514dc2c2c77874
Heesu Jeong <huissppea@gmail.com>
2022-06-02 19:39:32 +0900

    구조체 적용
    구조체 적용

--- favorites.h
+++ favorites.h
@@ -8,66 +8,75 @@
 #include "callendar.h"
 #include "crud.h"
 
-void search2(char fname[30000000], char word[100]);
-
 int favoritesList[99999999];
 
+typedef struct _Search {   
+	char word[100];
+	char numChar[1000];
+} Search;
+
+
+void search2(Search s1);
+
+
 int favorites(void) {
+
+	printf("-----------------즐겨찾기 파일목록-----------------\n");
 
 	for (int i = 0; i < sizeof(favoritesList) / sizeof(favoritesList[0]); i++) {
 		if (favoritesList[i] == NULL) {
 			continue;
 		}
-		printf("-----즐겨찾기 파일목록-----\n");
 		printf("%d\n", favoritesList[i]);
-		printf("---------------------------\n");
 	}
-	
-		printf("즐겨찾기 내용을 보고 싶나요?\n");
-		FILE* read;
-		char line[1000];
-		char line2[1000];
-		char filename[15];
-		int i = 0;
+	printf("------------------------------------------------\n");
 
-		printf("원하는 년, 월, 일을 입력하시오 [yyyymmdd]: ");
-		scanf("%s", filename);
-		strcat(filename, ".txt");
-		read = fopen(filename, "r");
-		fflush(stdin);
-		while (fgets(line, 500, read) != NULL) {
-			sscanf(line, "%[^\n]", line2);
-			printf("%s", line);
-		}
-		fclose(read);
-		
+	
+
+	printf("즐겨찾기 내용을 보고 싶나요?\n");
+	FILE* read;
+	char line[1000];
+	char line2[1000];
+	char filename[15];
+	int i = 0;
+
+	printf("원하는 년, 월, 일을 입력하시오 [yyyymmdd]: ");
+	scanf("%s", filename);
+	strcat(filename, ".txt");
+	read = fopen(filename, "r");
+	fflush(stdin);
+	while (fgets(line, 500, read) != NULL) {
+		sscanf(line, "%[^\n]", line2);
+		printf("%s", line);
+	}
+	fclose(read);
+
 	return 0;
 }
 
 
 // 탐색할 단어 찾기(1번째 과정)
 void search(void) {
-
-	char word[100];
+	Search s1;
 
 	printf("탐색할 단어: ");
-	scanf("%s", word);
+	scanf("%s", &s1.word);
 
 	printf("------------------------------------------------");
 
 
-	for (int i = 20000000; i < 21000000; i++) {	// 있는 파일을에서 특정 단어 찾기
+	for (int i = 20000000; i < 21000000; i++) {	// 있는 파일에서 특정 단어 찾기
 		FILE* fp = NULL;
 
 		char buffer[1000];
 
 		int count = 0;
-		char numChar[100];
+		
 		//파일열기
 		// i 정수를 문자열로 바꿔서 합쳐야 함
-		sprintf(numChar, "%d", i);	// 정수를 문자열로 바꿈
-		strcat(numChar, ".txt");
-		search2(numChar, word);
+		sprintf(s1.numChar, "%d", i);	// 정수를 문자열로 바꿈
+		strcat(s1.numChar, ".txt");
+		search2(s1);
 
 	}
 
@@ -78,32 +87,32 @@ void search(void) {
 
 
 // 탐색할 단어 찾기(2번째 과정)
-void search2(char fname[30000000], char word[100]){
+void search2(Search s1) {
 
-	
+
 	FILE* fp = NULL;
 	char buffer[1000];
 	int count = 0;
-	fp = fopen(fname, "r");
+	fp = fopen(s1.numChar, "r");
 
 	if (fp == NULL) {
 		return 0;
 	}
-	
+
 	while (fgets(buffer, 1000, fp) != NULL) {
 		count++;
-		if (strstr(buffer, word) != NULL) {
+		if (strstr(buffer, s1.word) != NULL) {
 
 			printf("\n%d줄 : %s", count, buffer);
 
 		}
 	}
 
-	printf("\n%s에서 발견\n", fname);
+	printf("\n%s에서 발견\n", s1.numChar);
 	printf("------------------------------------------------");
 
 	fclose(fp);
-	
+
 	return 0;
 
 }
